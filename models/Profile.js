b20fb250c20262b17d788e80f39a00d69d442f3f
Timur Bobylev <timur.bobylev@yandex.ru>
2019-03-28 06:28:53 -0700

    Profile is ready

--- models/Profile.js
+++ models/Profile.js
@@ -62,5 +62,57 @@ const ProfileSchema = new Schema({
                 type:String
             }
         }
-    ]
-})
+    ],
+    education:[
+        {
+           School:{
+                type:String,
+                required:true
+            },
+            degree:{
+                type:String,
+                required:true
+            },
+            fieldofstudy:{
+                type:String,
+                required:true
+            },
+            from:{
+                type:Date,
+                required:true
+            },
+            to:{
+                type:Date,
+            },
+            current:{
+                type:Boolean,
+                default:false
+            },
+            description:{
+                type:String
+            }
+        }
+    ],
+    social:{
+        youtube:{
+            type:String
+        },
+        twitter:{
+            type:String
+        },
+        facebook:{
+            type:String
+        },
+        linkedin:{
+            type:String
+        },
+        instagram:{
+            type:String
+        }
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
