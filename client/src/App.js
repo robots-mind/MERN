3efa9bc0f30c09e637f0bbea3355febc94717bf6
Timur Bobylev <timur.bobylev@yandex.ru>
2019-03-29 12:47:17 -0700

    reboot

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
